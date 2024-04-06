38dcb3f0247b8bd117c8e0ea0371b6ef999254e8
jiminpark0215 <snowman.jimin0215@gmail.com>
2024-04-06 23:16:08 +0900

    practice for algorithm basic

--- src/codingPractice18.java
+++ src/codingPractice18.java
@@ -1,48 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.*;
 /*
 
-백준 2193
+백준 1065
 
 문제
-0과 1로만 이루어진 수를 이진수라 한다. 이러한 이진수 중 특별한 성질을 갖는 것들이 있는데, 이들을 이친수(pinary number)라 한다. 이친수는 다음의 성질을 만족한다.
-
-이친수는 0으로 시작하지 않는다.
-이친수에서는 1이 두 번 연속으로 나타나지 않는다. 즉, 11을 부분 문자열로 갖지 않는다.
-예를 들면 1, 10, 100, 101, 1000, 1001 등이 이친수가 된다. 하지만 0010101이나 101101은 각각 1, 2번 규칙에 위배되므로 이친수가 아니다.
-
-N(1 ≤ N ≤ 90)이 주어졌을 때, N자리 이친수의 개수를 구하는 프로그램을 작성하시오.
+어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다. 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다.
+N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 N이 주어진다.
+첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
 
 출력
-첫째 줄에 N자리 이친수의 개수를 출력한다.
+첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
 
  */
 
 public class codingPractice18 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        //N(1 ≤ N ≤ 90) 정수가 90자리 넘어가면 int 로 불가 -> long
-        //dp (왜냐면 dp[1] dp[2]가 정해져있음
-        long[] dp = new long[N+1]; //n자리 이친 수의 총 개수
+        String N = br.readLine();
+        int cnt=0;
 
-        dp[0] = 0;
-        dp[1] = 1;
-
-        // dp[n] n자리 수 끝에 0이 오는 경우 + 1이 오는 경우를 더하면 된다.
-        // n 자리에 0과 1 두 가지만 올 수 있다.
-        // n 자리에 0이 들어 오면, 앞에 0과 1 둘 다 들어올 수 있다.
-        // n 자리에 1이 들어 오면, 앞에 0만 들어올 수 있다.
-
-        for(int i=2; i<=N; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        //입력받은 N이 1자리나 2자리일 경우 자릿수별 값들끼리 항상 등차수열이 성립되기 때문에 100 N이 100 이하면 N개 만큼 한수 존재.
+        if(Integer.parseInt(N)<100){
+            cnt = Integer.parseInt(N);
         }
-        System.out.println(dp[N]);
+
+        else{
+            cnt = 99; //위에서 N이 100 이하일 경우를 나눠놨기 때문에 N이 100이상일 경우엔 99개를 미리 더해놓고 100 보다 큰 값에서 한수를 찾으면 된다.
+            for(int i=100;i<=Integer.parseInt(N);i++){
+                String str = String.valueOf(i);
+                String[] arr = str.split("");
+
+                int minus_1 = Integer.parseInt(arr[0]) - Integer.parseInt(arr[1]);
+                int minus_2 = Integer.parseInt(arr[1]) - Integer.parseInt(arr[2]);
+
+                if(minus_1 == minus_2){
+                    cnt++;
+                }
+
+            }
+        }
+
+        System.out.println(cnt);
+
     }
 }
